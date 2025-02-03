a084e5a54f09ed2d5d2549724c1753f62c16355e
abi-16-alt <debugtechno16@gmail.com>
2025-02-03 22:29:13 +0530

    Add files via upload

---
+++ 34/loop/index.js
@@ -0,0 +1,51 @@
+/*//FOR LOOP:
+console.log("HI");
+function forLoop() {
+    for(let i=0;i<=4;i++){
+        
+        console.log(i+"Gobika");
+    }
+}
+//forLoop();
+
+
+WHILE LOOP:
+let i=1;
+function whileLoop(){
+    while(i<=10){
+        console.log(i+"javascript");
+        i++;
+    }
+
+}
+//whileLoop();
+
+
+WHILE LOOP WITH ARGUMENTS:
+var a=1;
+var b=1;
+let i=1;
+function whileWithArg(a,b){
+    while(i<=10){
+        a=a+b;
+        console.log(a);
+        i++;
+    }
+
+}
+whileWithArg(a,b);
+
+
+//DO-WHILE LOOP:
+var a="HI";
+var b="Elavarasi";
+function doWhileLoop(a,b){
+    let j=0;
+    do{
+        console.log(a,b);
+        j++;
+
+    }while(j<=5)
+}
+doWhileLoop(a,b);
+*/
---
+++ 34/loop/printeven.js
@@ -0,0 +1,142 @@
+/*
+//print even numbers
+let i=1;
+for(i=1;i<=25;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    
+}
+
+
+
+//print odd numbers
+
+let i=0;
+for(i=0;i<=100;i++){
+    if((i%2)==1){
+        console.log(i)
+    }
+}
+
+
+
+//decrement
+
+let i=800;
+while(i>=600){
+    console.log(i);
+    i--;
+
+}
+
+
+//do while
+
+let i=800;
+do{
+    console.log(i);
+    i--;
+}while(i>=600)*
+
+
+//sumof all the numbers from 1 to 100
+let i=1;
+let a=0;
+for(i=1;i<=100;i++){
+    a=a+i;
+    }
+console.log(a);
+
+
+
+
+let i=1;
+let a=0;
+for(i=1;i<=50;i++){
+    a=a+i;
+    }
+console.log(a);
+
+/*
+
+let i=1;
+while(i<=100){
+    console.log(i);
+    i++;
+}
+
+
+/*let i=1;
+while(i<=100){
+
+    if(i%2!=0){
+        console.log(i);
+    }
+    i++;
+}
+
+//sum of all numbers ,break when the sum exceed 100
+
+let i=1;
+let a=0;
+for(i=1;i<=50;i++){
+    
+    if(a>100){
+        break;
+    }
+    a+=i;
+   }
+console.log(a);
+
+
+//skip the number that is diisible by 3
+
+let i=1;
+let a=0;
+for(i=1;i<=50;i++){
+    
+    if(i%3!==0){
+        a+=i;
+    }
+   }
+console.log(a);
+
+// Multiplication table 
+
+let num=5;
+
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=1;j++){
+        console.log(num+"x"+i+"="+num*i);
+    }
+}
+
+
+
+let num=5;
+
+for(let i=5;i<=15;i++){
+    for(let j=1;j<=1;j++){
+        console.log(num+"x"+i+"="+num*i);
+    }
+}
+
+
+
+let num=5;
+for(let j=1;j<=10;j++){
+
+    console.log(num+"x"+j+"="+num*j);
+}
+    
+
+//print all tables
+
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(i+"x"+j+"="+i*j);
+    }
+}
+
+*/
