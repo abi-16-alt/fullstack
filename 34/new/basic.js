49b72fb7005bca150359b455141bc0e004db40eb
abi-16-alt <debugtechno16@gmail.com>
2025-02-05 06:43:10 +0530

    Add files via upload

---
+++ 34/new/basic.js
@@ -0,0 +1,47 @@
+/*var a=34
+var b=20
+console.log(a+b)
+a++
+console.log(a)
+b=a++
+console.log(b)
+console.log(a)
+console.log(++a)
+
+//function
+function gobi()
+{
+    console.log("Hi Gobika");
+}
+gobi()
+//add using function parameters
+function add(x,y)
+{
+    console.log(x+y);
+}
+var a=45
+var b=76
+add(a,b)
+
+//stringadd
+
+var factor="vijay"
+var fplayer="dhoni"
+var fmovie="aram"
+favourite()
+
+function favourite()
+{
+console.log("Favourite Actor:"+factor)
+console.log("Favourite player:"+fplayer)
+console.log("Favourite movie:"+fmovie)
+}*/
+
+
+//return function
+function myname()
+{
+    return "Gobika";
+}
+var Name=myname()
+console.log(Name);
---
+++ 34/new/nod.js
@@ -0,0 +1,4 @@
+console.log("Hello");
+var os = require('os');
+console.log("platform:"+os.platform());
+console.log("Arch:"+os.arch());
