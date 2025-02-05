69703112c1c55bfa36c11e2031c9b6bd8e32ee88
abi-16-alt <debugtechno16@gmail.com>
2025-02-05 07:14:33 +0530

    Add files via upload

---
+++ javascript/array method/a.js
@@ -0,0 +1,8 @@
+let fav=['apple','grape','banana'];
+//console.log(fav.length);
+//console.log(fav.toString());
+//console.log(fav.at(2));
+//console.log(fav.join("|"));
+//console.log(fav.pop())
+console.log(fav.push('kiwi'));
+console.log(fav);
